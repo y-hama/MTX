5a8745b2b9b30fad027847bead1c3b6cec12e4c8
yutahamamoto <mail.y.hamamoto@gmail.com>
2021-05-22 00:10:50 +0900

    #

---
+++ MTXCore/BaseType/ndArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTXCore.BaseType
+{
+    public abstract class ndArray
+    {
+        protected ndArray(params int[] dim)
+        {
+            Create(new List<int>(dim));
+        }
+        protected ndArray(List<int> dim)
+        {
+            Create(dim);
+        }
+
+        private void Create(List<int> dim)
+        {
+
+        }
+    }
+}
